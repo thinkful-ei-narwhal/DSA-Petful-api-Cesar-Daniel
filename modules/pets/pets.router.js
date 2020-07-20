e18cdfbf4c46f3a5331e427bded8e5cc908cf6cc
Daniel Sottile <danielsottile@gmail.com>
2020-07-19 21:23:31 -0400

    fixed i think

--- modules/pets/pets.router.js
+++ modules/pets/pets.router.js
@@ -16,7 +16,7 @@ router.get('/', (req, res) => {
   return res.json(pets);
 });
 
-router.delete('/', json, (req, res) => {
+router.delete('/:type', json, (req, res) => {
   const {type} = req.params;
   Pets.dequeue(type);
 
